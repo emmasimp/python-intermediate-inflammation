5063614016891fd8f79180e4dc998dd04a9d27e7
emma <emmasimp@users.noreply.github.com>
2022-05-24 16:17:16 +0100

    Changes to models

--- inflammation/models.py
+++ inflammation/models.py
@@ -13,22 +13,31 @@ import numpy as np
 def load_csv(filename):
     """Load a Numpy array from a CSV
 
-    :param filename: Filename of CSV to load
+    param filename: Filename of CSV to load
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array."""
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array."""
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array"""
+    max_data = np.max(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max_data[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
