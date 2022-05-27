3f5850df9d6c70b8670350cccab59e65af87155d
emma <emmasimp@users.noreply.github.com>
2022-05-27 13:57:58 +0100

    Seciont 3 stuff

---
+++ tests/test_serializers.py
@@ -0,0 +1,18 @@
+from inflammation import models, serializers
+
+def test_patients_json_serializer():
+    patients = [
+        models.Patient('Alice', [models.Observation(i, i + 1) for i in range(3)]),
+        models.Patient('Bob', [models.Observation(i, 2 * i) for i in range(3)]),
+    ]
+
+    #  save and reload the data
+    output_file = 'patients.json'
+    serializers.PatientJSONSerializer.save(patients, output_file)
+    patients_new = serializers.PatientJSONSerializer.load(output_file)
+
+    for patient_new, patient in zip(patients_new, patients):
+        assert patient_new.name == patient.name
+        for obs_new, obs in zip(patient_new.observations, patient.observations):
+            assert obs_new.day == obs.day
+            assert obs_new.value == obs.value
